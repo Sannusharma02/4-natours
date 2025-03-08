251553bcea9499e519ef8587be9c3731da6b6bb7
Sannu Sharma <sannusharma02@gmail.com>
2025-03-08 17:04:54 +0530

    express middle ware

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.url);
   console.log('hello world!');
+  next();
+})
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
   next();
 })
 
