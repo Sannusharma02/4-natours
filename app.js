9659035ac032890169f869a456d6f8bb59ce96df
Sannu Sharma <sannusharma02@gmail.com>
2025-03-08 13:01:49 +0530

    app.get('/api/v1/tours/:id'

--- app.js
+++ app.js
@@ -1,29 +1,74 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
 
-// app.get('/', (req, res) => {
-//   // res.status(200).send('Hello from the server!');
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app:'Natours' });
-// })
-//
-// app.post('/', (req, res) => {
-//   res.status(200).send('You can post on to this endpoint...');
-// })
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours',(req,res)=>{
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours }
-  })
-})
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10); // parsing string to int as 10 decimal
+  const tour = tours.find(el => el.id === id);
+
+  // if(id>tours.length){
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tour.length,
+    data: {
+      tour }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  // Write the entire tours array (with the new tour added) to the file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours, null, 2),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'fail',
+          message: 'Could not update tours file',
+        });
+        return;
+      }
+
+      // If successful, send a response with the newly created tour
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
+
+
+
 
 const port = 3000;
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
 });
