22d2b84427ee6d7c6084ed30ddcace6eb119f045
Sannu Sharma <sannusharma02@gmail.com>
2025-03-08 14:48:26 +0530

    app.delete('/api/v1/tours/:id'

--- app.js
+++ app.js
@@ -82,6 +82,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+  if (parseInt(req.params.id, 10)>tours.length ) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    })
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 
 
 const port = 3000;
