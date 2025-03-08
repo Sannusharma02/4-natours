0521bbee7262747a49d1b3b514d5095244805110
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2025-03-08 06:51:39 +0530

    app.get('/api/v1/tours'

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server!');
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app:'Natours' });
-})
+// app.get('/', (req, res) => {
+//   // res.status(200).send('Hello from the server!');
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app:'Natours' });
+// })
+//
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post on to this endpoint...');
+// })
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post on to this endpoint...');
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours',(req,res)=>{
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours }
+  })
 })
 
 const port = 3000;
 app.listen(port,() => {
   console.log(`Server started on port ${port}...`);
 });
-
