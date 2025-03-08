4864c6facbb13df36892f6e30e807737c9a053ca
Sannu Sharma <sannusharma02@gmail.com>
2025-03-08 15:00:39 +0530

    organizing my app.js routes

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours }
   });
-});
+}
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = parseInt(req.params.id, 10); // parsing string to int as 10 decimal
   const tour = tours.find(el => el.id === id);
 
@@ -33,9 +32,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
     data: {
       tour }
   });
-});
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -63,9 +62,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+}
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
 
   if (parseInt(req.params.id, 10)>tours.length ) {
     return res.status(404).json({
@@ -80,9 +79,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...>'
     }
   });
-});
+}
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
 
   if (parseInt(req.params.id, 10)>tours.length ) {
     return res.status(404).json({
@@ -95,9 +94,18 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   });
-});
+}
 
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
 
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
