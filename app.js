cd6d5e3d1d886b419bef91ed018788f35c80c0d5
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2025-03-09 22:23:26 +0530

    LOCAL AND ATLAS HOSTED CON IN MONGODD

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ const userRouter=require('./routes/userRoutes');
 const app = express();
 
 // 1. Middleware
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('hello world!');
+  // console.log('hello world!');
   next();
 })
 
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+// //Local
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// console.log(DB);
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(con => {});
+
+// hosted
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+console.log(DB);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(con => {});
+
 const port = process.env.PORT || 3000;
+console.log(port);
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
 });
