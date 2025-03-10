eafb9df7c8b9a766dfbf88c3cf3364a962fc526a
Sannu Sharma <sannusharma02@gmail.com>
2025-03-10 10:31:26 +0530

    add doc in server

--- server.js
+++ server.js
@@ -6,12 +6,46 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 // hosted
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-console.log(DB);
-mongoose.connect(DB, {
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(con => {});
+}).then(con => {
+  console.log('DB Connection Successfully Connected!');});
+
+const tourScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  }
+});
+const Tour = mongoose.model('Tour',tourScheme);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 4.5
+})
+
+testTour.save().then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log(err);
+});
 
 const port = process.env.PORT || 3000;
 console.log(port);
