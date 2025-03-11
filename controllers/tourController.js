4d2c2955eb7cebc0a5f5eea84b62618f3bd60175
Sannu Sharma <sannusharma02@gmail.com>
2025-03-11 12:28:44 +0530

    exports.aliasTopTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('./../models/tourModel');
 const { query, json } = require('express');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = 5;
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+  next();
+}
+
 // TOURS
 exports.getAllTours = async (req, res) => {
   try{
