e17a27dfc47d14be419531fdf727a4be802c3d94
Sannu Sharma <sannusharma02@gmail.com>
2025-03-17 14:00:34 +0530

    const handleValidationErrorDB

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -12,12 +12,12 @@ const handleDuplicateFieldsDB = err => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
-// const handleValidationErrorDB = err => {
-//   const errors = Object.values(err.errors).map(el => el.message);
-//
-//   const message = `Invalid input data. ${errors.join('. ')}`;
-//   return new AppError(message, 400);
-// };
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -61,8 +61,9 @@ module.exports = (err, req, res, next) => {
     if (err instanceof mongoose.Error.CastError) err = handleCastErrorDB(err);
 
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
-    // if (error.name === 'ValidationError')
-    //   error = handleValidationErrorDB(error);
+    console.log(err.name);
+    if (err.name === 'ValidationError')
+      err = handleValidationErrorDB(err);
 
     sendErrorProd(err, res);
   }
