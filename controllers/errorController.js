41afc69bc4819fed06b00f83e03fa5203fd23339
Sannu Sharma <sannusharma02@gmail.com>
2025-03-17 08:24:41 +0530

    // Operational, trusted error: send message to client //Production error or other unknown error : don't want the clients to know

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -37,7 +37,7 @@ module.exports = (err,req, res, next) => {
   if(process.env.NODE_ENV === 'development') {
     sendErrorDev(err,res);
   } else if(process.env.NODE_ENV === 'production') {
-    let error ={ ...err}
+    let error = { ...err}
     if (error.name === 'CastError')  error = handleCastErrorDB(error);
     sendErrorPro(err,res);
   }
