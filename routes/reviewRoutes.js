8017d03d7051440b95fc1c0dcd65654f307da902
Sannu Sharma <sannusharma02@gmail.com>
2025-03-29 12:50:07 +0530

    router.use('/:tourId/reviews',reviewRouter);

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
-const router = express.Router();
 
-router.route('/')
+const router = express.Router({ mergeParams: true });
+
+//POST /tour/234fad4/reviews
+//POST /reviews
+
+router
+  .route('/')
   .get(reviewController.getAllReviews)
   .post(
     authController.protect,
