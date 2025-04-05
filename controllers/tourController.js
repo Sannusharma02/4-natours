9704b1ab1e4238b3376cfc6214b3e5297af0bcba
Sannu Sharma <sannusharma02@gmail.com>
2025-04-05 12:31:32 +0530

    api done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./../controllers/handlerFactory');
-const req = require('express/lib/request');
 const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
@@ -50,7 +49,7 @@ exports.getTourStats = catchAsync (async(req, res, next) => {
 
 exports.getMonthlyPlan = catchAsync (async(req, res, next) => {
     const year = parseInt(req.params.year);
-    console.log(Tour.schema.paths);
+
     const plan = await Tour.aggregate([
       {
         $unwind: '$startDates'
@@ -123,3 +122,47 @@ exports.getToursWithin = catchAsync( async(req, res, next) => {
   })
 
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng.',
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1]
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1
+      }
+    }
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances
+    }
+  });
+});
+
+
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -17,13 +17,18 @@ router
 router
   .route('/monthly-plan/:year')
   .get(authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
     tourController.getMonthlyPlan);
 
-router.route('/tours-within/:distance/center/:latlng/unit/:unit')
-  .get(tourController.getToursWithin)
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
 // /tours-within?distance=233,center=-40,45,unit=mi
 // /tours-within/233/center/-40,45/unit/mi
+
+router
+  .route('/distances/:latlng/unit/:unit')
+  .get(tourController.getDistances);
 
 router
   .route('/')
