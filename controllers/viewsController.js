b0dca984fd60c4cb9ee046c9adc5a5dfb477375e
Sannu Sharma <sannusharma02@gmail.com>
2025-04-07 11:32:19 +0530

    node ./dev-data/data/import-dev-user-data.js --delete

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,29 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview =catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+
+  // 3) render that template using tour data from 1)
   res.status(200).render('overview',{
     title: 'All Tours',
-  })
-}
+    tours,
+  });
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour',{
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data, for the requested tour(including reviews and guides
+  const tour = await Tour.findOne({slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'reviews rating user',
   })
-}
+  // 2) Build Template
+
+  // 3) Render Template using data 1)
+  res.status(200).render('tour',{
+    title: `${tour.name} Tour`,
+    tour,
+  })
+})
