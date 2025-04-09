b4fadfb2b712ac944eab3859df26f18159245b23
Sannu Sharma <sannusharma02@gmail.com>
2025-04-09 16:45:52 +0530

    Rendering Error Pages

--- public/js/login.js
+++ public/js/login.js
@@ -7,12 +7,12 @@ export const login = async (email, password) => {
     const res = await axios({
       method: 'POST',
       url: 'http://127.0.0.1:3000/api/v1/users/login',
-
       data: {
-        email: email,
-        password: password
+        email,
+        password
       }
     });
+
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
@@ -30,10 +30,10 @@ export const logout = async () => {
       method: 'GET',
       url: 'http://127.0.0.1:3000/api/v1/users/logout'
     });
-    if (res.data.status === 'success') {
-      location.reload(true);
-    }
+    if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
-    showAlert('error', 'Error logging out');
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
   }
 };
+
