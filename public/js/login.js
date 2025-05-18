bfb46bd404be9f3f131d47e3b39518ea8c9d0868
Sannu Sharma <99250959+Sannusharma02@users.noreply.github.com>
2025-05-19 02:23:50 +0530

    Play 200

--- public/js/login.js
+++ public/js/login.js
@@ -7,13 +7,14 @@ export const login = async (email, password) => {
     console.log("3",email, password);
     const res = await axios({
       method: 'POST',
-      url: `http://127.0.0.1:3000/api/v1/users/login`,
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
       data: {
         email,
         password
-      }
+      },
+      withCredentials: true
     });
-    console.log(res);
+    console.log(res.data)
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
